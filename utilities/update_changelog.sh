8780b3049d148d13440b9e3780ec3dcbffd14fe7
Jonathan Peres <jonathan.peres@protonmail.com>
2017-08-04 16:12:39 -0300

    Add script to update CHANGELOG
    Changelog file will be updated automatically via script. Even a pull
    request is done, now a changelog file must be add, with 'title' and
    'pull_request' values.
    
    This script will update CHANGELOG with new version, passed as argument,
    then delete all changelogs files.

---
+++ utilities/update_changelog.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd $(dirname $(readlink -f $0))
+cd ..
+
+source pdc/sh/utils/yaml.sh
+
+version=$1
+today=$(date +%Y-%m-%d)
+old_changelog=$(cat CHANGELOG.md)
+link_requests="https://github.com/jasperes/personal-distro-configurator/pull/"
+
+[ -z "$version" ] && echo "Error: Version must be informed" && exit 1
+
+echo "## ${version} (${today})" > CHANGELOG.md
+echo "" >> CHANGELOG.md
+
+for entry in changelogs/unreleased/*; do
+
+	while read -r line; do
+		declare "$(echo "${line}" | cut -d ':' -f1)"="$(echo ${line} | cut -d ':' -f2)"
+	done < "$entry"
+
+	[ ! -z "$pull_request" ] && pull_request="[#${pull_request# }](${link_requests}${pull_request# })]"
+
+	echo "- ${title# } ${pull_request}" >> CHANGELOG.md
+done
+
+echo "" >> CHANGELOG.md
+echo "$old_changelog" >> CHANGELOG.md
+
+rm changelogs/unreleased/*
