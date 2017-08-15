9e63ab30eebce93296c101d9077b4f629ab83263
Jonathan Peres <jonathan.peres@protonmail.com>
2017-08-15 13:47:44 -0300

    Merge pull request #4 from jasperes/plugins
    Base structure for plugins. Everything that must be in a plugin are commented.

--- pdc/sh/installers/gem.sh
+++ pdc/sh/installers/gem.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-function gem_install() {
-    pdc_test_command "gem"
-    gem install "${settings_gem[*]}"
-}
+## TODO: Move to a plugin
+
+#function gem_install() {
+#    pdc_test_command "gem"
+#    gem install "${settings_gem[*]}"
+#}
