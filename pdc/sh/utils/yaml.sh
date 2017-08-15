93249823c700b22750a385a93ec394d18841c53d
Jonathan Peres <jonathan.peres@protonmail.com>
2017-08-15 13:52:17 -0300

    Update YAML Script #5
    Update yaml parser script.

--- pdc/sh/utils/yaml.sh
+++ pdc/sh/utils/yaml.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Credits to https://github.com/jasperes/bash-yaml/
 function parse_yaml() {
+    local yaml_file=$1
     local prefix=$2
     local s
     local w
@@ -10,18 +12,22 @@ function parse_yaml() {
     w='[a-zA-Z0-9_]*'
     fs="$(echo @|tr @ '\034')"
 
-    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
-    awk -F"$fs" '{
-        indent = length($1)/2;
-        vname[indent] = $2;
-        for (i in vname) {if (i > indent) {delete vname[i]}}
-            if (length($3) > 0) {
-                vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-                printf("%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, $3);
-            }
-        }' |
-    sed 's/_=/+=/g'
+    (
+        sed -ne 's/--//g; s/\"/\\\"/g; s/\#.*//g; s/\s*$//g;' \
+            -e  "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+            -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
+        awk -F"$fs" '{
+            indent = length($1)/2;
+            if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
+            vname[indent] = $2;
+            for (i in vname) {if (i > indent) {delete vname[i]}}
+                if (length($3) > 0) {
+                    vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+                    printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
+                }
+            }' |
+        sed 's/_=/+=/g'
+    ) < "$yaml_file"
 }
 
 function pdc_create_variables() {
